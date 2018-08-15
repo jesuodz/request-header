dda631b4bb5fab95c821efd29345e9951cbd74ca
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-08-15 16:09:58 -0400

    Move header-request function to its own file

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const express = require('express'),
-    app = express();
+    app = express(),
+    requestHeader = require('./api/request-header.js'),
+    router = express.Router();
 
-app.get('/', (request, response) => {
-    const header = request.headers;
 
-    const data = {
-        "ipaddress": header['x-forwarded-for'],
-        "language": header["accept-language"],
-        "software": header["user-agent"]
-    }
-    console.log(data);
-});
+app.set('view engine', 'pug');
+
+// Set routes — All in one file
+router.route('/').get(requestHeader.data);
+app.use('/', router);
+
+// All non-routes
+app.get('*', (request, response) => {
+    response.render('error-template', {"message": "Error!", "goback": "maybe you want to go back to index"});
+})
 
 const server = app.listen(3000, (request, response) => {
     console.log('Listening on port ' + server.address().port);
