5540e2fae0d960468e864faf062ae2983323145d
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-08-15 16:34:19 -0400

    Set public styles

--- app.js
+++ app.js
@@ -3,10 +3,14 @@
 const express = require('express'),
     app = express(),
     requestHeader = require('./api/request-header.js'),
-    router = express.Router();
+    router = express.Router(),
+    path = require('path');
 
-
+// Set engine
 app.set('view engine', 'pug');
+
+// Pretty links
+app.use("/static", express.static(path.join(__dirname,('public'))));
 
 // Set routes — All in one file
 router.route('/').get(requestHeader.data);
@@ -14,7 +18,7 @@ app.use('/', router);
 
 // All non-routes
 app.get('*', (request, response) => {
-    response.render('error-template', {"message": "Error!", "goback": "maybe you want to go back to index"});
+    response.render('error-template', {"title": "Yikes!", "goback": "Maybe you want to go back to main page"});
 })
 
 const server = app.listen(3000, (request, response) => {
