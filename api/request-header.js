d10df747af36daca658e2109a9373209af58cb83
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-08-15 22:19:56 -0400

    fix formating

--- api/request-header.js
+++ api/request-header.js
@@ -4,7 +4,7 @@ module.exports = {
     parseHeader: (request) => {
         const header = request.headers;
         const data = {
-            "ipaddress": header['x-forwarded-for'],
+            "ipaddress": header['x-forwarded-for'].split(',')[0],
             "language": header["accept-language"],
             "software": header["user-agent"]
         }
