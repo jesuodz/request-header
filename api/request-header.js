12ade59830abb36910372b6a7bb5eaa788271f81
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-08-15 21:59:54 -0400

    Refactor request functions and routes

--- api/request-header.js
+++ api/request-header.js
@@ -1,14 +1,13 @@
 'use strict';
 
 module.exports = {
-    data: function(request, response) {
+    parseHeader: (request) => {
         const header = request.headers;
-    
         const data = {
             "ipaddress": header['x-forwarded-for'],
             "language": header["accept-language"],
             "software": header["user-agent"]
         }
-        response.render('index', {requestData : JSON.stringify(data)});
+        return data;
     }
-}
+};
--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,13 @@
 const router = require('express').Router(),
     requestHeader = require('../api/request-header.js');
 
-router.route('/').get(requestHeader.index);
-router.route('/api/whoami').get(requestHeader.whoami);
+router.route('/').get((request, response) => {
+    let data = requestHeader.parseHeader(request);
+    response.render('index', {"title": "API Project: Request Header Parser Microservice", requestData: JSON.stringify(data)});
+});
+router.route('/api/whoami').get((request, response) => {
+    response.send(requestHeader.parseHeader(request));
+});
 
 router.route('*').get((request, response) => {
     response.render('error-template', {"title": "Yikes!", "goback": "Maybe you want to go back to main page"});
