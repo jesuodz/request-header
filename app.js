dad241f593bb2c21653eb85dd9a9cc391a01017b
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-08-15 22:12:40 -0400

    Add favicon and titles
    - Titles should change between error and index pages.

--- app.js
+++ app.js
@@ -3,14 +3,17 @@
 const express = require('express'),
     app = express(),
     routes = require('./routes/routes.js'),
-    path = require('path');
+    path = require('path'),
+    favicon = require('serve-favicon');
 
 // Set engine
 app.set('view engine', 'pug');
 
 // Pretty links
 app.use("/static", express.static(path.join(__dirname,('public'))));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
+// Set routes
 app.use('/', routes);
 app.use('/api/whoami', routes);
 app.use('*', routes);
